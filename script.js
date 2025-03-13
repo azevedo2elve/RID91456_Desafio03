a495224de31fbc4bb6f8d84b60f7f028c86e7e19
gabrielazevedo-egsys <gabriel.silva@egsys.com.br>
2025-03-12 22:25:11 -0300

    testes na criação de tasks e ajustando alguns alinhamentos de textos e também gaps

--- script.js
+++ script.js
@@ -30,4 +30,8 @@ let createTask = (titleText, tagText, dateText) => {
 
 window.addEventListener('load', function() {
     createTask('Implementando as tarefas no To-Do', 'backend', 'Criado em: 12/03/2025');
+    createTask('Segunda tarefa do To-Do', 'frontend', 'Criado em: 12/03/2025');
+    createTask('Incrementando tarefas no To-Do', 'frontend', 'Criado em: 12/03/2025');
+    createTask('Task com um texto muito grande pra ver até aonde vai, para não dar problema de responsividade e quebrar toda o card da task, colocando mais texto na task até ver onde consigo chegar e tentar quebrar, para depois ajustar', 'como a tag também é colocada pelo usuário, vou fazer um teste também colocando uma tag gigante para ver até aonde da para ir. Testando mais um pouco para ver se vai quebrar o frontend da página, até agora nada de quebrar, mas vamos ver mais agora', 'Criado em: 12/03/2025');
+    createTask('Verificando até aonde vai o To-Do', 'frontend', 'Criado em: 12/03/2025');
 });
